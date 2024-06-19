623e29e7bc9979a883afd4137d2eea1e60a1ae6a
Rene Vreede <vreede.rene@gmail.com>
2024-06-19 22:40:03 +0200

    try catch for hotwidget to r

--- R/hotwidget_to_R.R
+++ R/hotwidget_to_R.R
@@ -7,21 +7,27 @@ hotwidget_to_R <- function(dat) {
   factor_cols <- map_chr(dat$data_types$factor, unlist)
   num_cols <- map_chr(dat$data_types$numeric, unlist)
 
-  map(
-    dat$data,
-    \(.x) {
-      as_tibble(
-        map(.x, \(.x) ifelse(is.null(.x),NA,.x)),
-        .name_repair = ~headers)
+  tryCatch(
+    map(
+      dat$data,
+      \(.x) {
+        as_tibble(
+          map(.x, \(.x) ifelse(is.null(.x),NA,.x)),
+          .name_repair = ~headers)
+      }
+    )|>
+      bind_rows() |>
+      mutate(
+        across(any_of(num_cols), as.numeric),
+        across(any_of(int_cols), as.integer),
+        across(any_of(date_cols), as.Date),
+        across(any_of(factor_cols), as.factor),
+        across(any_of(char_cols), as.character)
+      ),
+    error = function(e) {
+      cli::cli_alert_danger(paste(e))
+      return(invisible())
     }
-  )|>
-    bind_rows() |>
-    mutate(
-      across(any_of(num_cols), as.numeric),
-      across(any_of(int_cols), as.integer),
-      across(any_of(date_cols), as.Date),
-      across(any_of(factor_cols), as.factor),
-      across(any_of(char_cols), as.character)
-    )
+  )
 }
 
